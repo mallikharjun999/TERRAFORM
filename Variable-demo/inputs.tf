20af2a84c94d53386f57c39cee68b34afd4cc7f9
malli996600 <ecemallikharjun@gmail.com>
2024-05-29 21:29:50 +0530

    tfvars added

--- Variable-demo/inputs.tf
+++ Variable-demo/inputs.tf
@@ -18,9 +18,3 @@ resource "aws_instance" "demo_instance" {
   
 }
 
-#DEFINING OUTPUT VARIABLE TO EXPOSE THE PUBLIC IP OF EC2 INSTANCE
-output "public_ip" {
-    description = "public ip of ec2"
-    value = aws_instance.demo_instance.public_ip
-  
-}
